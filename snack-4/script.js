e6fcc03623293fa84c6cd7b5ecd97cee360d924b
Zarzuket <davide.bajo@gmail.com>
2021-08-27 16:10:54 +0200

    fix snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -1,13 +1,13 @@
 // Crea un array di numeri interi
 // e fai la somma di tutti gli elementi che sono in posizione (indice dell'array) dispari
 
-var numeri = [1 , 2 , 1 , 2 , 1, 2];
-
+// var numeri = [1 , 2 , 1 , 2 , 1, 2];
+var numeri = [2, 4, 6, 8, 10];
 
 var somma = 0;
 
 for (i=0; i<numeri.length; i++) {
-    if(numeri[i] % 2 == 1){
+    if([i] % 2 == 1){
         somma += numeri[i];
     }
 
---
+++ snack-5/main.js
@@ -0,0 +1,21 @@
+// alert("gl bruh")
+
+// Crea due array che hanno un numero di elementi diversi.
+// Aggiungi elementi casuali all’array che ha meno elementi,
+// fino a quando ne avrà tanti quanti l’altro.
+
+
+var big = [ 1 , 2 , 3 , 4 , 5]
+var small = [1 , 2 ,  3]
+
+// for( i=0; i <big.length; i++){
+//     console.log(big[i])
+// }
+
+// for ( i=0; i < small.length; i++){
+//     console.log(small[i]);
+// }
+
+// if ( big[i] > small[i] ){
+
+// }
